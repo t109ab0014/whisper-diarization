589978301d3517e8a74a3ffc59390e82dd27312f
ro9 <131231801+t109ab0014@users.noreply.github.com>
2023-10-17 21:45:57 -0500

    Update diarize.py

--- diarize.py
+++ diarize.py
@@ -77,7 +77,7 @@ whisper_model = WhisperModel(
 )
 
 # or run on GPU with INT8
-# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
 # model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
